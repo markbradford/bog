edabdefcdd9860f07f9aad16ff47de117877e1ad
markbradford <markbradfordbc@gmail.com>
2015-04-29 01:03:42 -0700

    recommiting bog assignment, correct directory

---
+++ db/migrate/20150428152339_create_creatures.rb
@@ -0,0 +1,11 @@
+class CreateCreatures < ActiveRecord::Migration
+  def change
+    create_table :creatures do |t|
+      t.string :name
+      t.integer :age
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
